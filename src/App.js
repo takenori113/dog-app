55e245744a4321311981776adb2890aefccdf7d0
Takenori <takenori113@gmail.com>
2024-04-26 16:45:36 +0900

    指定した枚数の犬画像を表示する機能を追加

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-const DogPictureDisplay = ({ imageUrl }) => {
-
+const DogPictureDisplay = ({ imageUrls }) => {
+  const datas =  imageUrls.map((url) => {
+   return <img  key={url}  src={url} alt="犬画像" className='picDisplay' />
+  });
+  console.log(datas);
   return (
-    <img src={imageUrl} alt="犬画像" className='picDisplay' />
+    datas
   );
 };
 
-const Buttons = ({ fetchDogImage, clearDogImage }) => {
+const Buttons = ({ fetchDogImage, clearDogImage ,imageNums,onImgNumChange}) => {
   return (
     <div>
       <FetchButton fetchDogImage={fetchDogImage} />
       <ClearButton clearDogImage={clearDogImage} />
+      <InputNumberButton imageNums={imageNums} onImgNumChange={onImgNumChange}/>
     </div>
 
+  );
+};
+const InputNumberButton = ({imageNums,onImgNumChange})=>{
+  return(
+    <input type='number' value={imageNums} onChange={(e)=>{onImgNumChange(e)} } max={50} min={1}/>
   );
 };
 
@@ -32,31 +40,40 @@ const ClearButton = ({ clearDogImage }) => {
 }
 
 function App() {
-  const [imageUrl, setImageUrl] = React.useState("");
+  const [imageUrls, setImageUrls] = React.useState([]);
+  const [imageNums, setImagenums] = React.useState(1);
 
   const fetchDogImage = async () => {
     try {
-      const url = "https://dog.ceo/api/breeds/image/random";
+      const url = `https://dog.ceo/api/breeds/image/random/${imageNums}`;
       const res = await fetch(url);
       const data = await res.json();
-      setImageUrl(data.message);
+      setImageUrls(data.message);
+      console.log(data.message);
     } catch (e) {
       console.log(e);
     }
   };
-
+  
+  const onImgNumChange =(e)=>{
+    if(e.target.value){
+      setImagenums(parseInt(e.target.value));
+    }else{
+      setImagenums(1);
+    }
+  }
   const clearDogImage = () => {
-    setImageUrl("");
+    setImageUrls([]);
   }
 
   React.useEffect(() => {
     fetchDogImage();
-  });
+  }, []);
 
   return (
     <div>
-      {imageUrl && <DogPictureDisplay imageUrl={imageUrl} />}
-      <Buttons fetchDogImage={fetchDogImage} clearDogImage={clearDogImage} />
+      {imageUrls && <DogPictureDisplay imageUrls={imageUrls} />}
+      <Buttons fetchDogImage={fetchDogImage} clearDogImage={clearDogImage} imageNums={imageNums} onImgNumChange={onImgNumChange} />
     </div>
 
   );
